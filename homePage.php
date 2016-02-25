749db42c4fa0bf26d2eef7c3572249666d12297b
phillyRose <emma.phillips@uconn.edu>
2016-02-25 14:34:56 -0500

    Update homePage.php

--- homePage.php
+++ homePage.php
@@ -6,9 +6,9 @@
 <html>
 	<head>
 		<title> idk </title>
+		<link rel = "stylesheet" type = "text/css" href ="./general.css" />
 		<link rel = "stylesheet" type = "text/css" href = "./homePage.css" />
 		<script src = "https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"> </script>
-		<link rel = "stylesheet" type = "text/css" href = "./homePage.css" />
 		<link href='https://fonts.googleapis.com/css?family=Raleway:400,100,200,300,500,700,600,800,900' rel='stylesheet' type='text/css'>
 		<script language = "JavaScript">
 		<!--
@@ -18,22 +18,60 @@
 	<body>
 		<div id="container">
 			<div id="header">
-				<h1>Hello, World!</h1>	
+				<div id="pageTitle">
+					<h1>Welcome to Free Time!</h1>	
+				</div>
+				<div id="signInForm">
+			  		<form action="###" method="post" id="###">
+				 		<div id="existingUsername">
+				 			<p>Username:</p>
+				 			<input type="text" name="username"></textarea>	
+				 			</p>
+				 		</div>
+				 		<div id="existingPassword">
+				 			<p>Password:</p>
+				 			<input type="password" name="passCode"/>
+				 		</div>	
+				 		<div id="existingSubmit">
+				 			<input type="submit" name="submit" value="Sign In" id="submit"/>
+				 		</div>	
+					</form>
+				</div>	
+				<!-- Sign in form end -->
 			</div>  
-			<!-- Heading end -->
-			<div id="signInForm">
-			  	<form action="###" method="post" id="###">
-				 	<div>
-				 		<textarea name="userName" placeholder="Username"></textarea>	
-				 	</div>
-				 	<div>
-				 		<input type="password" name="passCode"/>
-				 	</div>	
-				 <div>
-				 	<input type="submit" name="submit" value="submit" id="submit"/>
-				 </div>	
-			</form>
-			<!-- Sign in form end -->
+			<!-- Header end -->
+			<div id="homePageContent">
+				<div id ="aboutFreeTime">
+					<h1>Meet up with<br />your groups<h1/>
+					<h2>Compare availibility and see what<br /> times work for everyone.</h2>	
+				</div>
+				<!-- About section end -->
+				<div id="signUpForm">
+					<h2>REGISTER NEW ACCOUNT</h2>
+				  	<form action="###" method="post" id="###">
+				  		<div id="firstName">
+					 		<textarea name="firstName" placeholder="First Name"></textarea>	
+					 	</div> 
+					 	<div id="lastName">
+					 		<textarea name="lastName" placeholder="Last Name"></textarea>	
+					 	</div>
+					 	<div id="email">
+					 		<textarea name="email" placeholder="Email"></textarea>	
+					 	</div> 
+					 	<div id="username">
+					 		<textarea name="userName" placeholder="Username"></textarea>	
+					 	</div>
+					 	<div>
+					 		<p>Password:</p>
+					 		<input type="password" name="passCode"/>
+					 	</div>	
+					 <div id="signUp">
+					 	<input type="submit" name="submit" value="Sign Up" id="submit"/>
+					 </div>	
+					</form>
+				<!-- Sign up end -->
+				</div>	
+			<!-- Content end -->
 			<footer>
 				<p>Thomas Hammons & Emma Phillips &copy; 2016</p>
 			</footer>
