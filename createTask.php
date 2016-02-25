7ab17d16b284711d43e0c6fc5cdebf85f5613500
phillyRose <emma.phillips@uconn.edu>
2016-02-25 16:57:44 -0500

    Update createTask.php

--- createTask.php
+++ createTask.php
@@ -1,4 +1,3 @@
-
 <!-- THIS IS THE FIRST PAGE SOMEONE WOULD ENCOUNTER  -->
 <!-- IF YOU ARE NOT YET A REGISTERED USER THEN IT WILL BRING YOU TO A PAGE THAT WILL ASK YOU TO FILL OUT SOME INFO -->
 <!-- CSS: ./homeOne.css -->
@@ -10,97 +9,7 @@
 		<link rel = "stylesheet" type = "text/css" href = "./createTask.css" />
 		<script src = "https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"> </script>
 		<link href='https://fonts.googleapis.com/css?family=Raleway:400,100,200,300,500,700,600,800,900' rel='stylesheet' type='text/css'>
-		<script language = "JavaScript">
-		<!--
-			var r,k = 0;
-			var allDays = new Array();
-			while( k < 31 )
-			{
-				r = k+1;
-				allDays[k] = r;
-				k++;
-
-			}
-
-			var allMonths = ['Month' , 'January','February','March','April','May','June','July','August','September','October','Novenmber','December',];
-			function addAllMonths()
-			{
-				var i = 0;
-				var newMonth = null;
-				var monthName = null;
-				var fromList = null;
-				while( i < allMonths.length )
-				{
-					newMonth = document.createElement("option");
-					fromList = allMonths[i];
-					monthName = document.createTextNode( fromList );
-					newMonth.appendChild( monthName );
-					document.getElementById("userMonth").appendChild( newMonth );
-					i++;
-				}
-
-			}
-			function addAllMonthsAgain()
-			{
-				var a = 0;
-				var newMonthAgain = null;
-				var monthNameAgain = null;
-				var fromListAgain = null;
-				while( a < allMonths.length )
-				{
-					newMonthAgain = document.createElement("option");
-					fromListAgain = allMonths[a];
-					monthNameAgain = document.createTextNode( fromListAgain );
-					newMonthAgain.appendChild( monthNameAgain );
-					document.getElementById("userMonthAgain").appendChild( newMonthAgain );
-					a++;
-				}
-
-			}
-			function addAllDays() 
-			{
-				var j = 0;
-				var newDay = null;
-				var dayName =  null;
-				var fromListDay = null;
-				while( j < allDays.length )
-				{
-					newDay = document.createElement("option");
-					fromListDay = allDays[j];
-					dayName = document.createTextNode( fromListDay );
-					newDay.appendChild( dayName );
-					document.getElementById("userDay").appendChild( newDay );
-					j++; 
-				}
-			}
-			function addAllDaysAgain() 
-			{
-				var b = 0;
-				var newDayAgain = null;
-				var dayNameAgain =  null;
-				var fromListDayAgain = null;
-				while( b < allDays.length )
-				{
-					newDayAgain = document.createElement("option");
-					fromListDayAgain = allDays[b];
-					dayNameAgain = document.createTextNode( fromListDayAgain );
-					newDayAgain.appendChild( dayNameAgain );
-					document.getElementById("userDayAgain").appendChild( newDayAgain );
-					b++; 
-				}
-			}
-
-			function listPopulater()
-			{
-				addAllMonths();
-				addAllDays();
-				addAllMonthsAgain();
-				addAllDaysAgain();
-				
-			}
-
-		//-->
-		</script>		
+		<script src = "./createTask.js"> </script>		
 	</head>
 	<body onload = "listPopulater()" >
 		<div id="container">
