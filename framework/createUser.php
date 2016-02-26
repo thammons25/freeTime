1b7a99615188182deee119a0a24e06afd29dc29b
Thomas Hammons <thomas.hammons_jr@uconn.edu>
2016-02-25 19:37:42 -0500

    Update createUser.php

--- framework/createUser.php
+++ framework/createUser.php
@@ -1,6 +1,13 @@
-<!-- this creates new user from the updated form page. -->
+<!-- THIS PAGE IS ARRIVED AT BECAUSE SOMEONE FILLED OUT THE FORM ON ./homePage.php -->
+
+<!-- USER CREATED NEW ACCOUNT  -->
+
+<!-- USER DOES NOT EVERY ACTUALLY SEE THIS PAGE IT IS FOR REDIRECTION AND DATABASE PURPOSES ONLY -->
+
+<!-- HEY TOM LOOK HERE: BE SURE TO USE PREPARED PHP/SQL STATEMENTS EVERYWHERE FOR EVERYTHING OTHERWISE YOULL GET FUCKED-->
+
 <?php
-	$myConnect = mysqli_connect( "localhost:3306" , "SQL USER HERE" , "SQL PWORD HERE" , "Freetime" );
+	$myConnect = mysqli_connect( "localhost:3306" , "root" , "new_password" , "Freetime" );
 	if( !($myConnect) )
 	{
 		die("FAILED<br />" . mysqli_connect_error() );
@@ -17,12 +24,46 @@
 	$pWord = $_POST["passCode"];
 
 	$prepSQL->execute();
-	echo "<p>Successfully added to database 'Freetime'</pn>";
 	$prepSQL->close();
 
 
 	mysqli_close( $myConnect );
 ?>
+<html>
+	<head>
+		<title> idk </title>
+		<!-- FUNCTION REDIRECTS TO THE PAGE ON WHICH THE PROGRAM EXISTS -->
+		<script language = "JavaScript">
+		<!--
+			function ShowInfo()
+			{
+				window.location = "./createTask.php";
+			}
+
+		//-->
+		</script>
+		
+	</head>
+
+
+
+	<body onload = "ShowInfo()">
+
+	</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
