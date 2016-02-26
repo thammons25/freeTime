31160a3c733b0ea03b4ec57b6f15bb3f3d5269eb
Thomas Hammons <thomas.hammons_jr@uconn.edu>
2016-02-25 19:41:48 -0500

    Create functionButton.php
    -definitely not necessary
    -use as active guide while building pages 
    -contains javascript window.location functions that change window to page w/php+mysql functionality

---
+++ framework/functionButton.php
@@ -0,0 +1,70 @@
+<html>
+	<head>
+		<title> idk </title>
+		<link rel = "stylesheet" type = "text/css" href = "./?.css" />
+		<script src = "https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"> </script>
+		<script language = "JavaScript">
+		<!--
+			function GoHome()
+			{
+				window.location = "./homePage.php";
+			}
+
+			function DeleteAccount()
+			{
+				window.location = "./promptForDelete.php";
+			}
+
+			function ShowUsers()
+			{
+				window.location = "./showUsers.php";
+			}
+
+
+
+
+
+
+		//-->
+		</script>
+
+
+		
+		
+	</head>
+
+
+
+	<body>
+		<div>
+			<p>Return to home page</p>
+			<button onclick = "GoHome()">BACK HOME</button>
+		</div>
+		<div>
+			<p>Delete Account</p>
+			<button onclick = "DeleteAccount()">DELETE ACCOUNT</button>
+		</div>
+		<div>
+			<p>Show All Accounts</p>
+			<button onclick = "ShowUsers()">SHOW ACCOUNTS</button>
+		</div>
+
+
+	</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
