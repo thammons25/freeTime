208fae854e7878a597947647f2e42b58f3a3e015
Thomas Hammons <thomas.hammons_jr@uconn.edu>
2016-02-25 19:40:51 -0500

    Create promptForDelete.php
    -file probably not needed
    -use as guide for how actually confirm delete should work

---
+++ framework/promptForDelete.php
@@ -0,0 +1,66 @@
+<?php include './functionButton.php'; ?>
+<html>
+	<head>
+		<title> idk </title>
+		<link rel = "stylesheet" type = "text/css" href = "./?.css" />
+		<script src = "https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"> </script>
+		<script language = "JavaScript">
+		<!--
+
+
+
+
+
+
+		//-->
+		</script>
+
+
+		
+		
+	</head>
+
+
+
+	<body>
+		<form method = "post" action = "./deleteAccount.php">
+			<p>Reconfirm Email: <input type = "text" name = "email" /></p>
+			<p>Reconfirm Password: <input type = "text" name = "passCode" /></p>
+			<br />
+			<input type = "submit" value = "Delete Account" />
+		</form>
+		<?php
+			
+			
+
+
+
+		?>
+
+		<script language = "JavaScript">
+		<!--
+			
+			
+
+
+
+		//-->
+		</script>
+
+	</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
