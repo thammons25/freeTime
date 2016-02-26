746775fad2d821c9b0254d6229566e6657ca7113
phillyRose <emma.phillips@uconn.edu>
2016-02-25 19:25:00 -0500

    Update createTask.php

--- createTask.php
+++ createTask.php
@@ -1,3 +1,4 @@
+
 <!-- THIS IS THE FIRST PAGE SOMEONE WOULD ENCOUNTER  -->
 <!-- IF YOU ARE NOT YET A REGISTERED USER THEN IT WILL BRING YOU TO A PAGE THAT WILL ASK YOU TO FILL OUT SOME INFO -->
 <!-- CSS: ./homeOne.css -->
@@ -15,34 +16,33 @@
 		<div id="container">
 			<div id="header">
 				<div id="pageTitle">
-					<h1>Welcome to Free Time!</h1>	
+					<h1>FreeTime</h1>	
 				</div>
 			</div>  
 			<!-- Header end -->
 			<div id="howItWorks">
-				<h2>Thank you for logging in!</h2>
-				<p>You can now create a new task or join an existing one!</p>
+				<h2>How FreeTime works</h2>
+				<p>Now that you have logged in you can either create a new task or join an existing task.  If you are creating a new task please select a start and end date for when you would like to meet with your group.  Once you finish creating a new task you can send it to your group members so they can fill out their availability. If one of your group members has aslready made a task then select your availibility during that period of time.</p>
 			</div>	
 			<div id="newTask">
-				<h2>Create New Task!</h2>
+				<h2>Create New Task</h2>
 				<form action="#">
 					<div id="startDate">
-						<p>Please Select Start Month!</p>
+						<p>Please select earliest Free Time:</p>
 						<select name="newTaskStartMonth" id = "userMonth">	
 						</select>
-						<p>Please Select Start Day!</p>
 						<select name="newTaskStartDay" id = "userDay">	
 						</select>	
 						<!-- New Task Start Date End -->
 					</div>
 					<div id="endDate">
-						<p>Please Select End Month!</p>
+						<p>Please select latest Free Time:</p>
 						<select name="newTaskEndMonth" id = "userMonthAgain">	
 						</select>
-						<p>Please Select End Date</p>
 						<select name="newTaskEndDay" id = "userDayAgain">	
 						</select>
 					</div>
+					<input type="submit" value="Submit"/>
 				</form>
 				<!-- New Task End -->
 				<form action="#">
@@ -50,11 +50,11 @@
 				</form>	
 			</div>	
 			<div id="existingTask">
-				<h2>Join Existing Task!</h2>
+				<h2>Join Existing Task</h2>
 			</div>	
 			<footer>
-				<a class="resume" href="facebook.com"><img src="./text70.png"/></a>
-				<a class="resume" href="facebook.com"><img src="./text70.png"/></a>
+				<a class="resume" href="facebook.com"><img src="./resume.png"/></a>
+				<a class="resume" href="facebook.com"><img src="./resume.png"/></a>
 				<p>Thomas Hammons & Emma Phillips &copy; 2016</p>
 			</footer>
 		</div>
